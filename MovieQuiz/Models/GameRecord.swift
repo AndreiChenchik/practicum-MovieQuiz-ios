8db15a291a890dbf4ecbdfe1fe4e406de4d647f6
Andrei Chenchik <andrei@chenchik.me>
2022-08-12 20:16:19 +0200

    Add GameRecord to store records

---
+++ MovieQuiz/Models/GameRecord.swift
@@ -0,0 +1,23 @@
+//
+//  GameRecord.swift
+//  MovieQuiz
+//
+//  Created by Andrei Chenchik on 12/8/22.
+//
+
+import Foundation
+
+struct GameRecord: Codable, Comparable {
+    let correct: Int
+    let total: Int
+    let date: Date
+
+    var score: Double {
+        guard total != 0 else { return 0.0 }
+        return Double(correct) / Double(total)
+    }
+
+    static func < (lhs: GameRecord, rhs: GameRecord) -> Bool {
+        lhs.score < rhs.score
+    }
+}
