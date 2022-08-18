e6fe1485dc34b7a916ff409b819a087a5e08c60e
Andrei Chenchik <andrei@chenchik.me>
2022-08-18 17:30:24 +0200

    Create loader for getting movies from the network

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,52 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Andrei Chenchik on 18/8/22.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(
+        handler: @escaping (Result<MostPopularMovies, Error>) -> Void
+    )
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(
+            string: "https://imdb-api.com/en/API/MostPopularMovies/k_kiwxbi4y"
+        ) else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+
+        return url
+    }
+
+
+    func loadMovies(
+        handler: @escaping (Result<MostPopularMovies, Error>) -> Void
+    ) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(
+                        MostPopularMovies.self, from: data
+                    )
+
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
