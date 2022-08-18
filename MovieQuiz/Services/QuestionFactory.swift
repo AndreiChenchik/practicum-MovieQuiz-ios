08a41415ddd353a5fc34af2570c9f5ce2c89c831
Andrei Chenchik <andrei@chenchik.me>
2022-08-18 17:31:51 +0200

    Update Question factory to load data from the network

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -8,57 +8,100 @@
 import Foundation
 
 class QuestionFactory: QuestionFactoryProtocol {
+    private let moviesLoader: MoviesLoading
     private weak var delegate: QuestionFactoryDelegate?
 
-    private let questions = [
-        QuizQuestion(
-            image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 9,2
-        QuizQuestion(
-            image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 9
-        QuizQuestion(
-            image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 8,1
-        QuizQuestion(
-            image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 8
-        QuizQuestion(
-            image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 8
-        QuizQuestion(
-            image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true), // Настоящий рейтинг: 6,6
-        QuizQuestion(
-            image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false), // Настоящий рейтинг: 5,8
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false), // Настоящий рейтинг: 4,3
-        QuizQuestion(
-            image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false), // Настоящий рейтинг: 5,1
-        QuizQuestion(
-            image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false) // Настоящий рейтинг: 5,8
-    ]
+    private var movies: [MostPopularMovie] = []
 
-    init(delegate: QuestionFactoryDelegate) {
+//    private let questions = [
+//        QuizQuestion(
+//            image: "The Godfather",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 9,2
+//        QuizQuestion(
+//            image: "The Dark Knight",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 9
+//        QuizQuestion(
+//            image: "Kill Bill",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 8,1
+//        QuizQuestion(
+//            image: "The Avengers",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 8
+//        QuizQuestion(
+//            image: "Deadpool",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 8
+//        QuizQuestion(
+//            image: "The Green Knight",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true), // Настоящий рейтинг: 6,6
+//        QuizQuestion(
+//            image: "Old",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false), // Настоящий рейтинг: 5,8
+//        QuizQuestion(
+//            image: "The Ice Age Adventures of Buck Wild",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false), // Настоящий рейтинг: 4,3
+//        QuizQuestion(
+//            image: "Tesla",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false), // Настоящий рейтинг: 5,1
+//        QuizQuestion(
+//            image: "Vivarium",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false) // Настоящий рейтинг: 5,8
+//    ]
+
+    init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate) {
+        self.moviesLoader = moviesLoader
         self.delegate = delegate
     }
 
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let mostPopularMovies):
+                self.movies = mostPopularMovies.items
+                self.delegate?.didLoadDataFromServer()
+            case .failure(let error):
+                self.delegate?.didFailToLoadData(with: error)
+            }
+        }
+    }
+
     func requestNextQuestion() {
-        let index = Int.random(in: 0..<questions.count)
-        delegate?.didRecieveNextQuestion(question: questions[safe: index])
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            let index = Int.random(in: 0..<self.movies.count)
+
+            guard let movie = self.movies[safe: index] else { return }
+
+            var imageData = Data()
+
+            do {
+                imageData = try Data(contentsOf: movie.imageURL)
+            } catch {
+                print("Failed to load image")
+            }
+
+            let rating = Float(movie.rating) ?? 0
+
+            let text = "Рейтинг этого фильма больше чем 7?"
+            let correctAnswer = rating > 7
+
+            let question = QuizQuestion(
+                image: imageData, text: text, correctAnswer: correctAnswer
+            )
+
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.delegate?.didRecieveNextQuestion(question: question)
+            }
+        }
     }
 }
